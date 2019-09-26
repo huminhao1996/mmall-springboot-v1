6fc330a1f241de61ad2ab0ac73fb27e81afdeedc
hmh <454977355@qq.com>
2019-09-26 15:44:40 +0800

    hmh 2019年9月26日 15:40:13 创建mmall 项目

---
+++ src/main/java/com/imooc/mmall/service/ICategoryService.java
@@ -0,0 +1,18 @@
+package com.imooc.mmall.service;
+
+
+import com.imooc.mmall.common.ServerResponse;
+import com.imooc.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by geely
+ */
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+
+}
