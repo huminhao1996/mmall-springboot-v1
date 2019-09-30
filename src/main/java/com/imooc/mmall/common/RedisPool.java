5f0b4545bb9c12346d49f86c244ef262f4744bb8
hmh <454977355@qq.com>
2019-09-30 08:46:12 +0800

    2019-09-29 redis学习和配置 jsonUtil配置

---
+++ src/main/java/com/imooc/mmall/common/RedisPool.java
@@ -0,0 +1,46 @@
+package com.imooc.mmall.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @描述:
+ * @创建人: hmh
+ * @创建时间: 2019/9/29 16:02
+ * @公司: 杭州中车数字科技有限公司
+ */
+@Configuration
+@Slf4j
+public class RedisPool {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    public JedisPool redisPoolFactory() throws Exception {
+        log.info("JedisPool注入成功！！");
+        log.info("redis地址：" + host + ":" + port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+        // 连接耗尽时是否阻塞, false报异常,ture阻塞直到超时, 默认true
+        jedisPoolConfig.setBlockWhenExhausted(true);
+        // 是否启用pool的jmx管理功能, 默认true
+        jedisPoolConfig.setJmxEnabled(true);
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, 1000 * 2);
+        return jedisPool;
+    }
+}
--- src/main/java/com/imooc/mmall/common/ServerResponse.java
+++ src/main/java/com/imooc/mmall/common/ServerResponse.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 /**
  * Created by geely
  */
-@JsonSerialize(include =  JsonSerialize.Inclusion.NON_NULL)
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 //保证序列化json的时候,如果是null的对象,key也会消失
 public class ServerResponse<T> implements Serializable {
 
